b3fa93d5f2e5e7ece581f9ba4933f30a8d221b43
Mert Sefa AKGUN <git@msakg.com>
2022-12-27 01:52:28 +0300

    added: random_change_char.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-// #![allow(unused)]
+#![allow(unused)]
 
 use clap::Parser;
 
 use crate::prelude::*;
-use std::fs::read_dir;
+use rand::Rng;
 
 mod error;
 mod prelude;
@@ -12,18 +12,25 @@ mod utils;
 fn main() -> Result<()> {
     let args = Cli::parse();
 
-    for entry in read_dir("./")?.filter_map(|e| e.ok()) {
-        let entry: String = W(&entry).try_into()?;
-        println!("{entry:?}");
-    }
-
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
-
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
-        }
-    }
+    let size = &args.size.unwrap_or(12);
+    let width = &args.width.unwrap_or(12);
+    let term = &args.term.unwrap_or(true);
+    println!("{:?} {:?} {:?}", size, width, term);
 
     Ok(())
 }
+
+fn random_change_char(s: &str, c: char) -> String {
+    let mut rng = rand::thread_rng();
+    let mut s = s.to_string();
+    let mut i = 0;
+    while i < s.len() {
+        if rng.gen_bool(0.5) {
+            s.replace_range(i..i + 1, &c.to_string());
+        }
+        i += 1;
+    }
+    s
+}
+
+fn tree(heigh: Option<usize>, screen_width: Option<usize>) {}
